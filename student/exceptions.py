db0ff0d2c8bb84914548e21569a4a049d4783a62
Shubham Dighe <digs197@gmail.com>
2016-06-08 18:26:02 +0530

    Add error messages in a separate file

---
+++ student/exceptions.py
@@ -0,0 +1,8 @@
+NameError = Exception('Enter appropriate name')
+RollNoError = Exception('Enter valid Roll Number')   
+EmailError = Exception('Enter valid e-mail address')   
+AddressError = Exception('No. of characters in \'Address\' should be between 1-180')
+AboutMeError = Exception('\'About me\' should have less than 500 characters')
+DeptError = Exception('Enter a valid department')
+ExistsError = Exception('Roll number provided already exists')
+NotFoundError = Exception('Requested Roll Number does not exist')
